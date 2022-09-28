745ede40797bfdab0da2be8ce2006bd0ba2c5c16
Thiago Cariuska <tcariuska@abkaizen.com>
2022-09-28 18:03:00 +0100

    add request

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -1,7 +1,9 @@
-
+import requests
 
 def get_quotes():
-    pass
+    req = requests.get("https://1c22eh3aj8.execute-api.us-east-1.amazonaws.com/challenge/quotes")
+    return req.json()
 
 def get_quote(quote_number):
-    pass
+    req = requests.get(f"https://1c22eh3aj8.execute-api.us-east-1.amazonaws.com/challenge/quotes/{quote_number}")
+    return req.json()
