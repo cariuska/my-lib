0c79c6bad48778d90708e1e947f1c70cb4174a8d
Thiago Cariuska <tcariuska@abkaizen.com>
2022-09-28 15:55:29 +0100

    teste

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+class main:
+
+    def __init__(self):
+        pass
+
+
+    def console(self, text):
+        print(text)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="my-lib",
+    version="0.0.1",
+    packages=[],
+    python_requires='>=3.6.0',
+    install_requires=[
+    ],
+    extras_require={
+    }
+)
