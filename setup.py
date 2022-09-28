697afb19258e20d447917658a455cc39776e50d0
Thiago Cariuska <tcariuska@abkaizen.com>
2022-09-28 16:17:27 +0100

    alteração

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from setuptools import setup
 setup(
     name="mylib",
     version="0.0.1",
-    packages=[],
     python_requires='>=3.6.0',
     install_requires=[
         "requests"
     ],
     extras_require={
     },
-    packages=find_packages(include=['mylib', 'mylib.*']),
-
+    packages=['mylib', 'maylib.console']
 )
