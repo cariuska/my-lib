78f90f61d2b3ae5e73fbb9dd87d9f06545686a23
Thiago Cariuska <tcariuska@abkaizen.com>
2022-09-28 16:36:39 +0100

    change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="my-lib-teste",
-    version="0.0.1",
+    version="0.0.2",
     author="Thiago Cariuska",
     python_requires='>=3.8.0',
     install_requires=[
